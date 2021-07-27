680ca1411ce411da4d677cbb8233b02e92b07f95
Zenfoldium <ashishsingh1501@gmail.com>
2021-07-28 04:53:48 +0530

    update

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -4,6 +4,7 @@ from discord.ext.commands import Bot as BotBase
 from dotenv import load_dotenv
 from discord import Embed
 import os
+from datetime import datetime
 load_dotenv()
 
 PREFIX ="%"
@@ -52,14 +53,17 @@ class Bot(BotBase):
             channel = self.get_channel(848126501735890965)
             await channel.send("Now online")
 
-            embed = Embed(title="Now Online",description="KiM3X is online to Have fun")
+            embed = Embed(title="Now Online",description="KiM3X is online to Have fun",color=0xFF0000,timestamp=datetime.utcnow())
             fields= [("Name", "Value", "True"),
                      ("Another Field", "Just another field is this", "True"),
                      ("non-line","This is non inline fields","False")]
 
             for name, value, inline in fields:
                 embed.add_field(name=name, value=value,inline=inline)
-
+            embed.set_author(name="Ashish")
+            embed.set_footer(text="morancium")
+            embed.set_image(url="https://ggsc.s3.amazonaws.com/images/made/images/uploads/Jan_and_Maisie_200_266_int_c1-1x.jpg")
+            embed.set_thumbnail(url="https://images.unsplash.com/photo-1627366247844-b4b5df8854d8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80")
 
             await channel.send(embed=embed)
 
